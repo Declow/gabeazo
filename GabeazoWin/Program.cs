da273d7579e977508b0e90cdfce28e67656e3e6f
Ditlev Tower <ditlev.stjerne@gmail.com>
2019-05-31 20:10:31 +0200

    initial commit

---
+++ GabeazoWin/Program.cs
@@ -0,0 +1,126 @@
+﻿using GabeazoWin.Properties;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace GabeazoWin
+{
+    class Program
+    {
+
+        //[System.Runtime.InteropServices.DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        //private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        //[System.Runtime.InteropServices.DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        //[return: MarshalAs(UnmanagedType.Bool)]
+        //private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        //[System.Runtime.InteropServices.DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        //private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        //[System.Runtime.InteropServices.DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        //private static extern IntPtr GetModuleHandle(string lpModuleName);
+        //private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+
+        //private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        //{
+        //    using (Process curProcess = Process.GetCurrentProcess())
+        //    using (ProcessModule curModule = curProcess.MainModule)
+        //    {
+
+        //        return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+
+        //            GetModuleHandle(curModule.ModuleName), 0);
+
+        //    }
+        //}
+
+
+        //private static List<Keys> list;
+        //private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        //{
+
+        //    if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+
+        //    {
+
+        //        int vkCode = Marshal.ReadInt32(lParam);
+        //        list.Add((Keys)vkCode);
+
+        //        if (Keyboard.IsKeyDown(Key.LeftCtrl) && Keyboard.IsKeyDown(Key.LeftShift) && Keyboard.IsKeyDown(Key.A))
+        //        {
+        //            System.Windows.Application app = new System.Windows.Application();
+        //            app.Run(new WPFProgram());
+        //            app.Shutdown();
+        //        }
+
+        //        Console.WriteLine((Keys)vkCode);
+
+        //    }
+
+        //    return CallNextHookEx(_hookID, nCode, wParam, lParam);
+
+        //}
+
+        //private const int WH_KEYBOARD_LL = 13;
+        //private const int WM_KEYDOWN = 0x0100;
+        //private static LowLevelKeyboardProc _proc = HookCallback;
+        //private static IntPtr _hookID = IntPtr.Zero;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+
+            System.Windows.Application app = new System.Windows.Application();
+            app.Run(new WPFProgram());
+            app.Shutdown();
+        }
+    }
+
+    //public class AppContext : ApplicationContext
+    //{
+    //    private NotifyIcon trayIcon;
+    //    HotKey _hotKey;
+
+    //    public AppContext()
+    //    {
+    //        // Initialize Tray Icon
+    //        trayIcon = new NotifyIcon()
+    //        {
+    //            Icon = Icon.FromHandle(Resources.AppIcon.GetHicon()),
+    //            ContextMenu = new ContextMenu(new MenuItem[] {
+    //            new MenuItem("Exit", Exit)
+    //        }),
+    //            Visible = true
+    //        };
+
+    //        Console.WriteLine("Test hotkey");
+
+    //        _hotKey = new HotKey(Key.C, KeyModifier.Shift | KeyModifier.Win, OnHotKeyHandler);
+
+    //    }
+
+    //    private void OnHotKeyHandler(HotKey hotKey)
+    //    {
+    //        Console.WriteLine("Test hotkey");
+    //    }
+
+    //    void Exit(object sender, EventArgs e)
+    //    {
+    //        // Hide tray icon, otherwise it will remain shown until user mouses over it
+    //        trayIcon.Visible = false;
+
+    //        System.Windows.Forms.Application.Exit();
+    //    }
+    //}
+}
---
+++ GabeazoWin/WPFProgram.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace GabeazoWin
+{
+    class WPFProgram : Window
+    {
+
+        private System.Windows.Point startLocation;
+        private System.Windows.Point endLocation;
+        private readonly string filename = "imagedata.png";
+        private Canvas canvas;
+        private System.Windows.Shapes.Rectangle myRect = new System.Windows.Shapes.Rectangle();
+
+        public WPFProgram()
+        {
+            int screenLeft = SystemInformation.VirtualScreen.Left;
+            int screenTop = SystemInformation.VirtualScreen.Top;
+            int screenWidth = SystemInformation.VirtualScreen.Width;
+            int screenHeight = SystemInformation.VirtualScreen.Height;
+
+            this.Height = screenHeight+10;
+            this.Width = screenWidth+20;
+            this.Left = screenLeft-10;
+            this.Top = screenTop;
+
+            //this.Opacity = 0.01;
+            var bc = new BrushConverter();
+            this.Background = (System.Windows.Media.Brush)bc.ConvertFrom("#0100ffff");
+            this.AllowsTransparency = true;
+            canvas = new Canvas();
+            canvas.Opacity = 0.25;
+            this.Content = canvas;
+
+            this.ShowInTaskbar = false;
+            this.WindowStyle = WindowStyle.None;
+            this.Topmost = true;
+            //this.WindowState = WindowState.Maximized;
+
+            this.MouseDown += WPFProgram_MouseDown;
+            this.MouseUp += WPFProgram_MouseUp;
+            this.MouseMove += WPFProgram_MouseMove;
+
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Cross;
+            
+
+            myRect.Stroke = System.Windows.Media.Brushes.Black;
+            myRect.Fill = System.Windows.Media.Brushes.SkyBlue;
+            myRect.VerticalAlignment = VerticalAlignment.Center;
+            canvas.Children.Add(myRect);
+
+        }
+
+        private void WPFProgram_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            endLocation = Mouse.GetPosition(this);
+
+            if (startLocation.X != 0)
+            {
+                var size = new System.Windows.Size();
+
+                if (startLocation.X > endLocation.X && startLocation.Y > endLocation.Y)
+                {
+                    size.Width = startLocation.X - endLocation.X;
+                    size.Height = startLocation.Y - endLocation.Y;
+                    myRect.Height = size.Height;
+                    myRect.Width = size.Width;
+
+                    Canvas.SetTop(myRect, endLocation.Y);
+                    Canvas.SetLeft(myRect, endLocation.X);
+                }
+                else if (startLocation.X < endLocation.X && startLocation.Y > endLocation.Y)
+                {
+                    size.Width = endLocation.X - startLocation.X;
+                    size.Height = startLocation.Y - endLocation.Y;
+                    myRect.Height = size.Height;
+                    myRect.Width = size.Width;
+
+                    Canvas.SetTop(myRect, endLocation.Y);
+                    Canvas.SetLeft(myRect, startLocation.X);
+                }
+                else if (startLocation.X > endLocation.X && startLocation.Y < endLocation.Y)
+                {
+                    size.Width = startLocation.X - endLocation.X;
+                    size.Height = endLocation.Y - startLocation.Y;
+                    myRect.Height = size.Height;
+                    myRect.Width = size.Width;
+
+                    Canvas.SetTop(myRect, startLocation.Y);
+                    Canvas.SetLeft(myRect, endLocation.X);
+                }
+                else if (startLocation.X < endLocation.X && startLocation.Y < endLocation.Y)
+                {
+                    size.Width = endLocation.X - startLocation.X;
+                    size.Height = endLocation.Y - startLocation.Y;
+                    myRect.Height = size.Height;
+                    myRect.Width = size.Width;
+
+
+                    Canvas.SetTop(myRect, startLocation.Y);
+                    Canvas.SetLeft(myRect, startLocation.X);
+                }
+            }
+        }
+
+        private void WPFProgram_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            endLocation = Mouse.GetPosition(this);
+            this.Content = null;
+
+            canvas.Children.Remove(myRect);
+            canvas.Opacity = 0;
+            canvas.InvalidateVisual();
+            canvas.UpdateLayout();
+
+            Action emptyDelegate = delegate { };
+            canvas.Dispatcher.Invoke(emptyDelegate, DispatcherPriority.Render);
+            var size = GetSize();
+            SaveImage(startLocation, size);
+            UploadImage();
+            File.Delete(filename);
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void WPFProgram_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var pointToWindow = Mouse.GetPosition(this);
+            startLocation = new System.Windows.Point(pointToWindow.X, pointToWindow.Y);
+        }
+
+        public System.Windows.Size GetSize()
+        {
+            var size = new System.Windows.Size();
+            if (startLocation.X > endLocation.X && startLocation.Y > endLocation.Y)
+            {
+                size.Width = startLocation.X - endLocation.X;
+                size.Height = startLocation.Y - endLocation.Y;
+
+                var temp = startLocation;
+                startLocation = endLocation;
+                endLocation = temp;
+
+            }
+            else if (startLocation.X < endLocation.X && startLocation.Y > endLocation.Y)
+            {
+                size.Width = endLocation.X - startLocation.X;
+                size.Height = startLocation.Y - endLocation.Y;
+
+                var temp = startLocation;
+                startLocation.Y = endLocation.Y;
+                endLocation.Y = temp.Y;
+
+            }
+            else if (startLocation.X > endLocation.X && startLocation.Y < endLocation.Y)
+            {
+                size.Width = startLocation.X - endLocation.X;
+                size.Height = endLocation.Y - startLocation.Y;
+
+                var temp = startLocation;
+                startLocation.X = endLocation.X;
+                endLocation.X = temp.X;
+
+            }
+            else if (startLocation.X < endLocation.X && startLocation.Y < endLocation.Y)
+            {
+                size.Width = endLocation.X - startLocation.X;
+                size.Height = endLocation.Y - startLocation.Y;
+            }
+            return size;
+        }
+
+        private void SaveImage(System.Windows.Point location, System.Windows.Size size)
+        {
+            // create the bitmap to copy the screen shot to
+            Bitmap bitmap = new Bitmap(Convert.ToInt32(this.Width), Convert.ToInt32(this.Height));
+            // now copy the screen image to the graphics device from the bitmap
+            using (Graphics gr = Graphics.FromImage(bitmap))
+            {
+                gr.CopyFromScreen(new System.Drawing.Point(Convert.ToInt32(System.Windows.Application.Current.MainWindow.Left)
+                    ,Convert.ToInt32(System.Windows.Application.Current.MainWindow.Top))
+                    , new System.Drawing.Point(0, 0), new System.Drawing.Size(Convert.ToInt32(this.Width), Convert.ToInt32(this.Height)));
+                Console.WriteLine("test");
+            }
+
+            var rect = new Rectangle(Convert.ToInt32(location.X), Convert.ToInt32(location.Y), Convert.ToInt32(size.Width), Convert.ToInt32(size.Height));
+            var cropped = bitmap.Clone(rect, bitmap.PixelFormat);
+            cropped.Save(filename, ImageFormat.Png);
+        }
+
+        private void UploadImage()
+        {
+            const string url = "http://gabeazo.com/gabeazo.php";
+
+            using (WebClient client = new WebClient())
+            {
+                var server = new Uri(url);
+                client.Encoding = Encoding.UTF8;
+                
+                var res = client.UploadFile(server, filename);
+
+                string response = Encoding.UTF8.GetString(res);
+                System.Windows.Clipboard.SetText(response);
+                System.Diagnostics.Process.Start(response);
+            }
+        }
+    }
+}
